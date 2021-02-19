3328382855d8ad75b58f906ebb8c296f550da23a
Ahmad Ayubi <aayub041@uottawa.ca>
2021-02-19 13:45:16 -0500

    Initial formatter and readme update, still needs work

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # pygeoapi-mapml-formatter
-MapML Formatter Plugin for pygeoapi 
+
+MapML Formatter Plugin for pygeoapi
+
+## Installation
+
+1. Install pygepapi
+2. Copy `mapml.py` into pygeoapi/pygeoapi/formatter
+3. Add `'mapml'` to the [`FORMATS` array](https://github.com/geopython/pygeoapi/blob/master/pygeoapi/api.py#L79) on line 79.
+4. Copy the below snipping into lines [1090](https://github.com/geopython/pygeoapi/blob/master/pygeoapi/api.py#L1110) and [1248](https://github.com/geopython/pygeoapi/blob/master/pygeoapi/api.py#L1263) of pygeoapi/api.py. Appending to the existing `if` `else if` blocks,
+
+    ```python
+    elif format_ == 'mapml':
+      formatter = load_plugin('formatter', {'name': 'MapML', 'geom': True})
+      content = formatter.write(
+          data=content,
+          options={}
+      )
+
+      headers_['Content-Type'] = '{}; charset={}'.format(
+          formatter.mimetype, self.config['server']['encoding'])
+
+      return headers_, 200, content
+    ```
+
+5. Add `'MapML': 'pygeoapi.formatter.mapml.MapMLFormatter'` to the formatters array on line [55](https://github.com/geopython/pygeoapi/blob/master/pygeoapi/plugin.py#L55) in the pygeoapi/plugin.py file
+6. Build pygeoapi as you would before, now you can use `?f=mapml` to serve mapml features.
---
+++ mapml.py
@@ -0,0 +1,61 @@
+import logging
+from xml.etree.ElementTree import Element, SubElement, tostring
+
+from pygeoapi.formatter.base import BaseFormatter
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MapMLFormatter(BaseFormatter):
+    """MapML formatter"""
+
+    def __init__(self, formatter_def):
+        super().__init__({'name': 'mapml', 'geom': None})
+        self.mimetype = 'text/mapml'
+
+    def write(self, options={}, data=None):
+        """
+        Generate data in MapML format
+        :returns: string representation of format
+        """
+        mapml = Element('mapml', {'xmlns':'https://www.w3.org/1999/xhtml/'})
+        head = SubElement(mapml, 'head')
+        title = SubElement(head, 'title')
+
+        body = SubElement(mapml, 'body')
+
+        if data['type'] == 'FeatureCollection':
+            for f in data['features']:
+                body.append(self.__generateFeature(f))
+        elif data['type'] == 'Feature':
+            body.append(self.__generateFeature(data))
+        
+        return tostring(mapml)
+
+    def __generateFeature(self, f):
+        type = f['geometry']['type'].lower()
+        featureElem = Element('feature')
+        properties = SubElement(featureElem, 'properties')
+        name = SubElement(properties, 'h1')
+
+        name.text = f['properties']['name'] if 'name' in f['properties'] else f['properties']['stn_id']
+        geometry = SubElement(featureElem, 'geometry')
+                    
+        geometrySubtype = SubElement(geometry, type)
+        if type == 'multipolygon':
+            for p in f['geometry']['coordinates']:
+                polygon = SubElement(geometry, 'polygon')
+                polygon.text = self.__coordToStringPair(p)
+        else:
+            geometrySubtype.text = self.__coordToStringPair(f['geometry']['coordinates'])
+
+        return featureElem
+
+    def __coordToStringPair(self, coords):
+        if type(coords[0]) == list:
+            cString = ''
+            for pair in coords[0]:
+                cString += str(pair[0]) + ' ' + str(pair[1]) + ' '
+            return cString[:-1]
+        else: 
+            return str(coords[0]) + ' ' +  str(coords[1])
